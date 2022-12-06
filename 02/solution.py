621e20120228aaca8dff523a899fd66beb3f3690
ben-blakemore <blakemoreb1000@gmail.com>
2022-12-06 20:37:27 +0000

    Day 2

--- 02/solution.py
+++ 02/solution.py
@@ -4,15 +4,38 @@ values = {
     "rock" : 3
     }
 
-my
+elf_map = {
+    "A" : "rock",
+    "B" : "paper",
+    "C" : "scissors"
+}
+
+my_map = {
+    "X" : "rock",
+    "Y" : "paper",
+    "Z" : "scissors"
+}
 
 def main():
     with open("input.txt", "r") as f:
-        for line in f.readLines():
-            opponentChoice = line[0]
-            myChoice = line[2]
-            print(opponentChoice)
-            print(myChoice)
+        score = 0
+        for line in f.readlines():
+            opponentChoiceRaw = line[0]
+            myChoiceRaw = line[2]
+            score += calculate_score(opponentChoiceRaw, myChoiceRaw)
+        print(score)
+
+def calculate_score(opp, mine):
+    # oppChoiceScore = values[elf_map[opp]]
+    myChoiceScore  = values[my_map[mine]]
+    myChoice = my_map[mine]
+    oppChoice = elf_map[opp]
+    if (myChoice ==  "rock" and oppChoice == "scissors") or (myChoice == "paper" and oppChoice == "rock") or (myChoice == "scissors" and oppChoice == "paper"):
+        return myChoiceScore + 6
+    elif myChoice == oppChoice:
+        return myChoiceScore + 3
+    else:
+        return myChoiceScore
 
 if __name__ == "__main__":
     main()
